71f162693ab181411fb31e80550e639be8c9bee6
sshopin <sshopin@mail.ru>
2026-06-10 19:02:04 +0300

    feat: print start date/time

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python
 
+import datetime
+utc_now = datetime.datetime.now(datetime.timezone.utc)
+datetime_str = utc_now.strftime("%y.%m.%d %H:%M:%S")
+print(f'Started: {datetime_str}')
+
 print("Hello!")
