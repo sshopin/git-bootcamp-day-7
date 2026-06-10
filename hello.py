0d9a289e931b300e63fb9152df3cba116993b69d
sshopin <sshopin@mail.ru>
2026-06-10 19:03:10 +0300

    feat: error handling

--- hello.py
+++ hello.py
@@ -5,4 +5,14 @@ utc_now = datetime.datetime.now(datetime.timezone.utc)
 datetime_str = utc_now.strftime("%y.%m.%d %H:%M:%S")
 print(f'Started: {datetime_str}')
 
-print("Hello!")
+try:
+    print("Hello!")
+except Exception as e:
+    print(e)    
+    pass
+finally:
+    # This block is optional but ensures cleanup if needed
+    print('Finalization') 
+
+
+ 
